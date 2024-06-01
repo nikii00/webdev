618c38a0316671f799e542d920fa66e5f0ea4ac6
nikii00 <abelanikita22@gmail.com>
2024-06-01 08:39:51 +0800

    add env

--- web/utils/db.js
+++ web/utils/db.js
@@ -3,10 +3,10 @@ import mysql from 'mysql2/promise';
 
 export async function query(sql, values) {
   const db = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'aa',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
   });
   const [results] = await db.execute(sql, values);
   db.end();
