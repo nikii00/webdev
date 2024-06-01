f9b9ef0b1d0422e5e1f467d3eb243b31fd3e5b67
nikii00 <abelanikita22@gmail.com>
2024-06-01 09:46:11 +0800

    env

--- web/utils/db.ts
+++ web/utils/db.ts
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const dbConfig = {
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'aa',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 };
 
 export const connectToDatabase = async () => {
